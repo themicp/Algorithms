9f09bd0c7cec3a034dc2b342f0b7b6d974a62e58
Themis Papameletiou <themicp@gmail.com>
2011-12-15 20:52:07 +0000

    Books problem

--- bookcopy/bookcopy.cpp
+++ bookcopy/bookcopy.cpp
@@ -3,94 +3,61 @@
 
 using namespace std;
 
-void initializeBooks( int *B, int K, int left, int i ) {
-    if ( left == 0 ) {
-        return;
+bool validResult( int *A, int K, int N, int value ) {
+    int current = 0, i = 0, j = 0;
+
+    while ( j < K ) {
+        current = 0;
+
+        while ( current + A[ i ] <= value && i < N - ( K - ( j + 1 ) ) ) {
+            current += A[ i ];
+
+            ++i;
+        }
+
+        if ( A[ i ] > value && i < N ) {
+            return false;
+        }
+
+        ++j;
+    }
+
+    if ( i < N ) {
+        return false;
+    }
+
+    return true;
+}
+
+int binarySearch( int *A, int K, int N, int a, int low, int high ) {
+    int mid = ( low + high )/2;
+
+    if ( low == high ) {
+        return low + 1;
+    }
+
+    if ( validResult( A, K, N, mid + 1 ) ) {
+        binarySearch( A, K, N,  a, low, mid );
     }
     else {
-        ++B[ i ];
-        initializeBooks( B, K, left - 1, i + 1 );
-    }
-}
-
-void initializeLength( int *B, int K, int *A, int *lens ) {
-    int i = 0, j, it = 0;
-    while( i < K ) {
-        lens[ i ] = 0;
-        j = 0;
-        while ( j < B[ i ] ) {
-            lens[ i ] += A[ it ];
-            ++it;
-            ++j;
-        }
-        ++i;
-    }
-}
-
-int max( int *lens, int K ) {
-    int max = 0, i;
-    for ( i = 0; i < K; ++i ) {
-        if ( lens[ i ] > max ) {
-            max = lens[ i ];
-        }
-    }
-    return max;
-}
-
-void nextComb( int *B, int K, int *A, int *lens, int i, int &total ) {
-    int temp = B[ i ], j, temp_max = 0;
-    if ( i + 1 < K ) {
-        while ( B[ i ] > 1 ) {
-            nextComb( B, K, A, lens, i + 1, total );
-            initializeLength( B, K, A, lens );
-            temp_max = max( lens, K );
-            if ( total == 0 ) {
-                total = temp_max;
-            }
-            else if ( temp_max < total ) {
-                total = temp_max;
-            }
-            --B[ i ];
-            ++B[ i + 1 ];
-        }
-        nextComb( B, K, A, lens, i + 1, total );
-        initializeLength( B, K, A, lens );
-        temp_max = max( lens, K );
-        if ( total == 0 ) {
-            total = temp_max;
-        }
-        else if( temp_max < total ) {
-            total = temp_max;
-        }
-        B[ i ] = temp;
-        B[ i + 1 ] = 1;
+        binarySearch( A, K, N,  a, mid + 1, high );
     }
 }
 
 FILE *in = fopen( "bookcopy.in", "r" ), *out = fopen( "bookcopy.out", "w" );
-int N, K, *A, each, left, *B, *lens, i, total;
+int N, K, *A, i, total, maxValue;
 
 int main() {
     fscanf( in, "%i %i", &N, &K );
 
     A = ( int* )malloc( N * sizeof( int ) );
-    B = ( int* )malloc( K * sizeof( int ) );
-    lens = ( int* )malloc( K * sizeof( int ) );
 
     for ( i = 0; i < N; ++i ) {
         fscanf( in, "%i", &A[ i ] );
+        maxValue += A[ i ];
     }
 
-    B[ 0 ] = N - ( K - 1 );
-    for ( i = 1; i < K; ++i ) {
-        B[ i ] = 1;
-    }
-
-    initializeLength( B, K, A, lens );
-
-    nextComb( B, K, A, lens, 0, total );
-
-    fprintf( out, "%i\n", total );
+    fprintf( out, "%i\n", binarySearch( A, K, N, maxValue, 0, maxValue - 1 ) );
 
     return 0;
 }
