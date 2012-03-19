3b35efacdcdb043de13e9f9764f14bbf35f7183a
Themis Papameletiou <themicp@gmail.com>
2012-03-19 23:04:06 +0200

    Longest Increasing Subsequence O( NlgN )

--- lis/lis.cpp
+++ lis/lis.cpp
@@ -5,54 +5,66 @@
 
 using namespace std;
 
-struct Number {
-    int value;
-    int w;
-};
+#define INF 987654321
 
 FILE *in = fopen( "lis.in", "r" );
-int N, i, j, w, k;
-Number *A;
-int *seq;
-pair< int, int > result;
-stack< int > res;
+int N, L, i, j, *M, *X, *P;
+
+int BS( int L, int debug, int key ) {
+    int i, j, maxj = 0, mid;
+    i = 1; 
+    j = N;
+    while ( i <= j ) {
+        mid = ( i + j )/2;
+        if ( X[ M[ mid ] ] < X[ key ] && mid <= L ) {
+            maxj = max( maxj, mid );
+            i = mid + 1;
+        }
+        else {
+            j = mid - 1;
+        }
+    }
+    return maxj;
+}
 
 int main() {
     fscanf( in, "%d", &N );
-    A = ( Number* )malloc( N * sizeof( Number ) );
-    seq = ( int* )malloc( N * sizeof( int ) );
-    for ( i = 0; i < N; ++i ) {
-        fscanf( in, "%d", &A[ i ].value );
-        A[ i ].w = 0;
+    M = new int[ N + 1 ];
+    X = new int[ N + 1 ];
+    P = new int[ N + 1 ];
+
+    for ( i = 1; i <= N; ++i ) {
+        fscanf( in, "%d", &X[ i ] );
     }
 
-    for ( i = 0; i < N; ++i ) {
-        w = 0;
-        k = -1;
-        for ( j = 0; j < i; ++j ) {
-            if ( A[ i ].value > A[ j ].value ) {
-                if ( A[ j ].w + 1 > w ) {
-                    k = j;
-                    w = A[ j ].w + 1;
-                }
-            }
-        }
-        A[ i ].w = max( 1, w );
-        seq[ i ] = k;
-        if ( A[ i ].w > result.first ) {
-            result.first = A[ i ].w;
-            result.second = i;
+    L = 0;
+    M[ 0 ] = 0;
+    X[ 0 ] = INF;
+    for ( i = 1; i <= N; ++i ) {
+        j = BS( L, 0, i );
+        P[ i ] = M[ j ];
+        if ( j == L || X[ i ] < ( X[ M[ j + 1 ] ] && M[ j + 1 ] != INF ) ) {
+            L = max( L, j + 1 );
+            M[ j + 1 ] = i;
         }
     }
-    printf( "%i\n", result.first );
-    i = result.second;
-    while ( i != -1 ) { 
-        res.push( A[ i ].value );
-        i = seq[ i ];
+
+    stack< int > res;
+    j = M[ L ];
+    for ( i = 1; i <= L; ++i ) {
+        res.push( X[ j ] );
+        j = P[ j ];
     }
+
     while ( !res.empty() ) {
         printf( "%i ", res.top() );
         res.pop();
     }
+    printf( "\n" );
+
+    delete[] M;
+    delete[] X;
+    delete[] P;
+
     return 0;
 }
