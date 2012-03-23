a60455f5ff3234d17ce87c89a3559ed8dca7a3b4
Themis Papameletiou <themicp@gmail.com>
2012-03-23 21:12:42 +0200

    Lines DP problem

---
+++ lines/lines.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+#include <utility>
+
+using namespace std;
+
+struct Newline {
+    double w;
+    int left;
+};
+
+double w;
+char **word;
+Newline *nline;
+int N, C, i, j, k, left, sum;
+FILE *in = fopen( "lines.in", "r" );
+
+int main() {
+    fscanf( in, "%i %i", &N, &C );
+
+    word = new char*[ N ];
+    nline = ( Newline* )malloc( N * sizeof( Newline ) );
+
+    for ( i = 0; i < N; ++i ) {
+        word[ i ] = new char[ C + 1 ];
+        nline[ i ].w = pow( ( double )C, 2 ) * N;
+        fscanf( in, "%s\n", word[ i ] );
+    }
+
+    for ( i = 0; i < N; ++i ) {
+        j = i;
+        sum = 0;
+        left = C - strlen( word[ j ] );
+        while ( left >= 0 && j >= 0 ) {
+            w = pow( ( double )left, 2 );
+            w += ( j > 0 ) ? nline[ j - 1 ].w : 0;
+            nline[ i ].w = min( nline[ i ].w, w );
+
+            sum = 0;
+            --j;
+            if ( j >= 0 ) {
+                for ( k = j; k <= i; ++k ) {
+                    sum += strlen( word[ k ] );
+                    sum += ( k != i ) ? 1 : 0;
+                }
+                left = C - sum; 
+            }
+        }
+    }
+
+    printf( "%.0lf\n", nline[ N - 1 ].w );
+    return 0;
+}
