29b6698f6a31c1bca84dea960b20a79f2f9df956
Themis Papameletiou <themicp@gmail.com>
2012-03-29 17:08:13 +0300

    Counting shortest paths

--- shortestpath/count/count.cpp
+++ shortestpath/count/count.cpp
@@ -7,22 +7,23 @@ using namespace std;
 const int INF = 987654321;
 
 FILE *in = fopen( "../path.in", "r" );
-int i, cntV, cntE, u, v, neighbor[ 2005 ], d, S, T, counter;
+int i, cntV, cntE, u, v, neighbor[ 2005 ], *d, S, T, counter, top;
 int V[ 2005 ][ 2005 ];
-bool marked[ 2005 ];
-vector< pair< int, pair< int, int > > > E( 1 );
-queue< pair< int, int > > frontier;
-pair< int, int > top;
+bool done;
+queue< int > frontier;
 
 int main() {
     fscanf( in, "%i %i %i %i", &cntV, &cntE, &S, &T );
     --S;
     --T;
 
-    E.resize( cntE );
+    d = new int[ cntV ];
+    for ( i = 0; i < cntV; ++i ) {
+        d[ i ] = INF;
+    }
 
     for ( i = 0; i < cntE; ++i ) {
-        fscanf( in, "%i %i %*i", &u, &v );
+        fscanf( in, "%i %i", &u, &v );
         --u;
         --v;
         V[ u ][ neighbor[ u ] ] = v;
@@ -31,34 +32,36 @@ int main() {
         ++neighbor[ v ];
     }
 
-    marked[ S ] = 1; 
+    d[ S ] = 0; 
     for ( i = 0; i < neighbor[ S ]; ++i ) {
-        frontier.push( make_pair( 1, V[ S ][ i ] ) );
+        frontier.push( V[ S ][ i ] );
+        d[ V[ S ][ i ] ] = 1;
+        if ( V[ S ][ i ] == T ) {
+            counter = 1;
+            done = true;
+            break;
+        }
     }
 
-    d = INF;
-    while ( !frontier.empty() ) {
+    while ( !frontier.empty() && !done ) {
         top = frontier.front();
         frontier.pop();
-        if ( top.second == T ) {
-            if ( d == top.first ) {
-                ++counter;
+        for ( i = 0; i < neighbor[ top ]; ++i ) {
+            u = V[ top ][ i ];
+            if ( d[ top ] + 1 <= d[ u ] ) {
+                if ( u != T ) {
+                    frontier.push( u );
+                }
+                else {
+                    counter = d[ top ] + 1 < d[ u ] ? 1 : counter + 1;
+                }
             }
-            else {
-                counter = d < top.first ? counter : 1;
-            }
-            d = min( d, top.first );
+            d[ u ] = min( d[ top ] + 1, d[ u ] );
         }
-        for ( i = 0; i < neighbor[ top.second ]; ++i ) {
-            u = V[ top.second ][ i ];
-            if ( !marked[ u ] ) {
-                frontier.push( make_pair( top.first + 1, u ) );
-            }
-        }
-        marked[ top.second ] = 1;
     }
     printf( "Possible shortest paths: %i\n", counter );
-    printf( "Shortest path: %i\n", d );
+    printf( "Shortest path: %i\n", d[ T ] );
 
+    delete[] d;
     return 0;
 }
