c80cb485785d4d531a6fa26d4df532cb62dfc94e
themicp <themicp@gmail.com>
2014-11-28 12:31:55 +0200

    Fix the algorithm logic

--- graph_traversal/bfs/bfs.cpp
+++ graph_traversal/bfs/bfs.cpp
@@ -6,26 +6,32 @@
 using namespace std;
 
 FILE *in = fopen( "bfs.in", "r" );
-int M, N, i, j, u, v, **E, *V;
+int M, N, i, j, u, v, **E, *V, startNode, endNode, pathLength = 0, *minPath;
 queue< int > A;
 
-void BFS( int node, int needle ) {
+int BFS( int startNode, int endNode ) {
     int i, front;
 
-    if ( node == needle ) {
-        printf( "Needle found.\n" );
-        return;
+    if ( startNode == endNode ) {
+        return minPath[ endNode ];
     }
     for ( i = 0; i < N; ++i ) {
-        if ( E[ node ][ i ] && !V[ i ] ) {
-            V[ i ] = 1;
-            A.push( i );
+        if ( E[ startNode ][ i ] ) {
+            minPath[ i ] = min( minPath[ i ], minPath[ startNode ] + 1 );
+            if ( !V[ i ] ) {
+                V[ i ] = 1;
+                A.push( i );
+            }
         }
     }
     if ( A.size() ) {
         front = A.front();
         A.pop();
-        BFS( front, needle );
+        ++pathLength;
+        return BFS( front, endNode );
+    }
+    else {
+        return -1;
     }
 }
 
@@ -46,12 +52,15 @@ void printEdges() {
 
 int main() {
     fscanf( in, "%i %i", &M, &N );
+    fscanf( in, "%i %i", &startNode, &endNode );
 
     E = ( int** )malloc( N * sizeof( int* ) );
     V = ( int* )malloc( N * sizeof( int ) );
+    minPath = ( int* )malloc( N * sizeof( int ) );
     for ( i = 0; i < N; ++i ) {
         E[ i ] = ( int* )malloc( N * sizeof( int ) );
         V[ i ] = 0;
+        minPath[ i ] = M + 1;
 
         for ( j = 0; j < N; ++j ) {
             E[ i ][ j ] = 0;
@@ -60,11 +69,12 @@ int main() {
 
     for ( i = 0; i < M; ++i ) {
         fscanf( in, "%i %i", &u, &v );
-        E[ u - 1 ][ v - 1 ] = 1;
-        E[ v - 1 ][ u - 1 ] = 1;
+        E[ u ][ v ] = 1;
+        E[ v ][ u ] = 1;
     }
 
-    V[ 0 ] = 1;
-    BFS( 0, 4 );
+    V[ startNode ] = 1;
+    minPath[ startNode ] = 0;
+    printf( "Path length: %i\n", BFS( startNode, endNode ) );
     return 0;
 }
